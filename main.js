2a54830b76f32b729fb2f0d2dd5a1f0719dad869
grigoryan.m <waldm4rk@gmail.com>
2023-08-30 14:54:37 +0400

    added spacebar hotkey

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const res1 = document.getElementsByClassName("res-field")[0];
     const res2 = document.getElementsByClassName("res-field")[1];
     const button = document.getElementById("button");
-    
+
     button.addEventListener("click", () => {
         if(res2.value != null && res1.value != null){
             const gcd = (a, b) => {
@@ -21,6 +21,26 @@ document.addEventListener("DOMContentLoaded", () => {
             
             asp1.value = res1.value / commonDivisor;
             asp2.value = res2.value / commonDivisor;
+        }
+    });
+    document.addEventListener("keydown", (event) => {
+        if(event.keyCode === 13){
+            event.preventDefault();
+            if(res2.value != null && res1.value != null){
+                const gcd = (a, b) => {
+                    while (b !== 0) {
+                        const temp = b;
+                        b = a % b;
+                        a = temp;
+                    }
+                    return a;
+                };
+            
+                const commonDivisor = gcd(res1.value, res2.value);
+                
+                asp1.value = res1.value / commonDivisor;
+                asp2.value = res2.value / commonDivisor;
+            }
         }
     });
 });
